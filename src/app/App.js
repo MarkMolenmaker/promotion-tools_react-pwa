bb583e493af87d140807b2ae609d0f59f762b3f4
inazumark <inazumarkmail@gmail.com>
2021-04-22 21:58:22 +0200

    PWA Setup

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { MDBContainer } from 'mdb-react-ui-kit';
+
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <MDBContainer fluid>
+            </MDBContainer>
+        )
+    };
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
+import App from './app/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
